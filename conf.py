10c93911e3bb43585bb8b2ab980d5e9f23260ce1
chad-iris <chad.trabant@iris.edu>
2022-10-02 16:10:56 -0700

    Remove unneeded jsonschema and sphinx_toolbox

--- conf.py
+++ conf.py
@@ -39,8 +39,6 @@ master_doc = 'index'
 extensions = [
   'sphinx_rtd_theme',
   'sphinxmark',
-  'sphinx-jsonschema',
-  'sphinx_toolbox.collapse',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
